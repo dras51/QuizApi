235ac4fcb12e09d85507f6abc0008bbc36898ffc
David Olabode <davvido51@gmail.com>
2019-11-24 08:53:56 +0100

    adding list.js for questions again

---
+++ src/modules/question/controller/actions/list.js
@@ -0,0 +1,28 @@
+import Question from 'questionModule/model/index';
+import APIfeatures from 'util/APIfeatures';
+
+const listQuestion = async (req, res) => {
+  try {
+    const features = new APIfeatures(Question.find(), req.query)
+      .filter()
+      .sort()
+      .LimitFields()
+      .paginaton();
+
+    const questions = await features.query;
+    res.status(200).json({
+      status: 'success',
+      results: questions.length,
+      data: {
+        questions
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err.message
+    });
+  }
+};
+
+export default listQuestion;
