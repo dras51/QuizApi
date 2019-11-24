ed83caaf6a45bcab7d4c3da29638aff26fe0898d
David Olabode <davvido51@gmail.com>
2019-11-24 19:42:36 +0100

    changed all file extensions

--- src/models/User.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable global-require */
-export const crypto = require('crypto');
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({});
-
-const User = mongoose.model('User', userSchema);
-
-export default User;
--- src/util/modelRelationships.ts
+++ src/util/modelRelationships.ts
@@ -7,40 +7,45 @@
     name: 'Users'
  * }
  */
-export const hasOne = (schema, options) => {
+export type options = {
+  ref: string;
+  type: any;
+  name: string;
+};
+const hasOne = (schema: any, options: options) => {
   const fieldInString = `{ "${options.name}" : { "ref": "${options.ref}"}}`;
   const field = JSON.parse(fieldInString);
-  field[options.name].type = options.type;
+  field[options.name]["type"] = options.type;
   schema.add(field);
 };
 
-export const belongsTo = (schema, options) => {
+const belongsTo = (schema: any, options: options) => {
   const fieldInString = `{ "${options.name}" : { "ref": "${options.ref}"}}`;
   const field = JSON.parse(fieldInString);
-  field[options.name].type = options.type;
+  field[options.name]["type"] = options.type;
   schema.add(field);
 };
 
-export const mustBelongTo = (schema, options) => {
+const mustBelongTo = (schema: any, options: options) => {
   const fieldInString = `{ "${options.name}" : { "ref": "${options.ref}"}}`;
   const field = JSON.parse(fieldInString);
-  field[options.name].type = options.type;
-  field[options.name].required = true;
+  field[options.name]["type"] = options.type;
+  field[options.name]["required"] = true;
   schema.add(field);
 };
 
-export const hasMany = (schema, options) => {
+const hasMany = (schema: any, options: options) => {
   const fieldInString = `{ "${options.name}" : [{ "ref": "${options.ref}"}]}`;
   const field = JSON.parse(fieldInString);
-  field[options.name][0].type = options.type;
-  field[options.name][0].default = [];
+  field[options.name][0]["type"] = options.type;
+  field[options.name][0]["default"] = [];
   schema.add(field);
 };
 
-export const belongsToMany = (schema, options) => {
+const belongsToMany = (schema: any, options: options) => {
   const fieldInString = `{ "${options.name}" : [{ "ref": "${options.ref}"}]}`;
   const field = JSON.parse(fieldInString);
-  field[options.name][0].type = options.type;
-  field[options.name][0].default = [];
+  field[options.name][0]["type"] = options.type;
+  field[options.name][0]["default"] = [];
   schema.add(field);
 };
